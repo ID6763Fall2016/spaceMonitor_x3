080fa39dcc507053fe26a089c58836ff370b767f
Cheryl Wang <cherylwang@lawn-128-61-14-59.lawn.gatech.edu>
2016-09-20 17:30:24 -0400

    Add temp

--- test.js
+++ test.js
@@ -4,6 +4,25 @@
 var ads1x15 = require('node-ads1x15');
 var chip = 1;
 var adc = new ads1x15(chip);
+var raspi = require('raspi');
+var I2C = require('raspi-i2c').I2C;
+var i2c = new I2C();
+raspi.init(function(){
+//	var i2c= new I2C();
+	console.log(i2c.readByteSync(0x40));
+});
+
+setInterval(function(){
+	var raw_data = i2c.readWordSync(0x40);
+	console.log("raw_data");
+	console.log(raw_data);
+	var temp = (raw_data >> 2 ) * 0.03125;
+	
+	console.log("DieTemp")
+	console.log(temp);
+}, 1000);
+
+
 
 var channel = 0;
 var samplesPerSecond = '250';
@@ -13,7 +32,7 @@ var reading = 0;
 setInterval(function(){
    if(!adc.busy){
 	adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, function(err, data) {
-		console.log(data[1]);
+		console.log(data);
 		});  
 	}
 	
