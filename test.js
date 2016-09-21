4fe74f0d51e66bf28a8531896c746830647897c7
Cheryl Wang <cherylwang@cheryls-mbp.attlocal.net>
2016-09-20 23:35:35 -0400

    Adding log.

--- test.js
+++ test.js
@@ -3,8 +3,7 @@ var raspi = require('raspi');
 var I2C = require('raspi-i2c').I2C;
 var i2c = new I2C();
 raspi.init(function(){
-	var i2c= new I2C();
-	console.log(i2c.readByteSync(0x40));
+	console.log("Raspberry Pi Initializing...");
 });
 
 
