60bb936654be8614965bbd34eebc07cd5b1e77ee
Cheryl Wang <cherylwang@cheryls-mbp.attlocal.net>
2016-09-21 13:40:39 -0400

    Fix typo.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 // var app = require('http').createServer(handler);
 var server = require('http').Server(app);
-var io = require('socket.io')(serve);
+var io = require('socket.io')(server);
 // var fs = require('fs');
 var path = require('path');
 
