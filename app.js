b7e153df1574602fb215b78582d2205e4001f5df
Cheryl Wang <cherylwang@cheryls-mbp.attlocal.net>
2016-09-21 13:28:08 -0400

    Add path.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
+var path = require('path');
 
 var Engine = require('tingodb')(),
 assert = require('assert');
